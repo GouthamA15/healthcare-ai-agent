e6c8fb73e56135926236746925f1853339c723ad
GouthamA15 <lucky.masna1@gmail.com>
2026-03-21 15:26:16 +0530

    Vector Store

--- vectorstore/embeddings.py
+++ vectorstore/embeddings.py
@@ -1,14 +1,31 @@
 from langchain_community.embeddings import HuggingFaceEmbeddings
+from functools import lru_cache
 
+@lru_cache(maxsize=1)
 def get_embedding_model():
     """
-    Returns the Embedding Model used for vector generation.
+    Loads and returns the embedding model.
+    Uses caching to prevent reloading the model multiple times
     """
     
-    model_name = "sentence-transformers/all-MiniLM-L6-v2"
-
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+    
+    #Device Configuration (CPU for Now)
+    model_kwargs={
+        "device": "cpu"
+    }
+    
+    #Encoding Configuration 
+    encode_kwargs={
+        "normalize_embeddings": True
+    }
+    
+    #Initializing embedding model
     embeddings = HuggingFaceEmbeddings(
-        model_name=model_name        
+        model_name=model_name,
+        model_kwargs=model_kwargs,
+        encode_kwargs=encode_kwargs
     )
     
     return embeddings
+    
--- vectorstore/vector_store.py
+++ vectorstore/vector_store.py
@@ -6,11 +6,20 @@ from langchain_community.vectorstores import Chroma
 from vectorstore.embeddings import get_embedding_model
 
 
-DATA_PATH = "data/medical_docs"
+DATA_PATH = "data"
 CHROMA_PATH = "vectorstore/chroma_db"
 
 
 def load_documents():
+    
+    #Documents get stroed in this format:
+    """
+    [
+        Document(page_content="This is the page data", metadata={"source":"pdf", "page": 1}),
+        Document(page_content="This is the next page data", metadata={"source":"pdf", "page": 2})
+    ]    
+    """
+    
     documents = []
 
     for file in os.listdir(DATA_PATH):
@@ -24,10 +33,22 @@ def load_documents():
 def split_documents(documents):
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=500,
-        chunk_overlap=100
+        chunk_overlap=50
     )
 
     chunks = text_splitter.split_documents(documents)
+    #Now chunks would look like this:
+    """
+    [
+        Document(page_content="small piece of first page", metadata={"source":"pdf", "page": 1}),
+        Document(page_content="second piece of first page", metadata={"source":"pdf", "page": 1}),
+        Document(page_content="third piece of first page", metadata={"source":"pdf", "page": 1}),
+        Document(page_content="forth piece of first page", metadata={"source":"pdf", "page": 1}),
+        Document(page_content="small piece of second page", metadata={"source":"pdf", "page": 2})
+    ]
+    """
+    
+    
     return chunks
 
 
@@ -35,6 +56,9 @@ def create_vector_store():
 
     documents = load_documents()
 
+    if not documents:
+        raise ValueError("No PDF Documents were found in data folder")
+    
     chunks = split_documents(documents)
 
     embedding_model = get_embedding_model()
@@ -48,6 +72,10 @@ def create_vector_store():
     vectordb.persist()
 
     print("Vector database created successfully")
+    print(f"Loaded {len(documents)} documents")
+    print(f"Created {len(chunks)} chunks")
+    
+    return vectordb
 
 
 if __name__ == "__main__":
